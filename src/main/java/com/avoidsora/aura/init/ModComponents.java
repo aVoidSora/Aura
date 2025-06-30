46bd4ad34c18b74d2822d651e8f14176f83af15b
avoidsora <Lordderpysquirtle@gmail.com>
2025-06-30 11:30:14 -0400

    Init

--- src/main/java/com/avoidsora/aura/init/ModComponents.java
+++ src/main/java/com/avoidsora/aura/init/ModComponents.java
@@ -2,11 +2,8 @@ package com.avoidsora.aura.init;
 
 import com.avoidsora.aura.Aura;
 import com.avoidsora.aura.aura.AuraComponent;
-
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.entity.EntityType;
-import net.minecraft.world.entity.player.Player;
-
 import net.neoforged.neoforge.capabilities.EntityCapability;
 import net.neoforged.neoforge.capabilities.RegisterCapabilitiesEvent;
 
@@ -14,7 +11,7 @@ public class ModComponents {
     public static final ResourceLocation AURA_ID = Aura.id("aura");
 
     public static final EntityCapability<AuraComponent, Void> AURA =
-            EntityCapability.create(AURA_ID, AuraComponent.class, Void.class); // ✅ FIXED
+            EntityCapability.create(AURA_ID, AuraComponent.class, Void.class);
 
     public static void registerCapabilities(RegisterCapabilitiesEvent event) {
         event.registerEntity(AURA, EntityType.PLAYER, (player, ctx) -> new AuraComponent(100));
